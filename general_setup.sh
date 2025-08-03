c34fbec39cda890d581b765d9bf33d15473b5546
byte-master92 <149981421+byte-master92@users.noreply.github.com>
2025-08-03 08:42:53 -0400

    Update general_setup.sh

--- general_setup.sh
+++ general_setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # general_setup.sh
 
-# Civicomfy custom node
-mkdir -p ComfyUI/custom_nodes
-cd ComfyUI/models/custom_nodes
+# Civicomfy and Art Venture custom nodes for ComfyUI
+
+mkdir -p /ComfyUI/custom_nodes
+cd /ComfyUI/custom_nodes
 git clone https://github.com/MoonGoblinDev/Civicomfy.git || echo "Civicomfy already cloned"
 git clone https://github.com/sipherxyz/comfyui-art-venture.git || echo "Art Venture already installed"
 cd -
